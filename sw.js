367d74b531028194ce31fc04e7a45a8ff8f99e4c
carlorura82 <ilcaminotignale@gmail.com>
2026-04-19 21:37:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,23 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installato');
+const CACHE_NAME = 'pos-v1';
+// Usa percorsi relativi (./) per evitare il 404 su GitHub Pages
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Lascia passare le richieste normalmente
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
